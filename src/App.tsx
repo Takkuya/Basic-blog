f6e485589fbc008671243fc8ebc29e49cbee3af9
Takkuya <takuyagabriel@gmail.com>
2022-03-21 12:31:22 -0300

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Header } from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      Hello World
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import { HeaderContainer } from "./styles";
+
+export const Header = () => {
+  return (
+    <HeaderContainer>
+      <h1>Hello World</h1>
+    </HeaderContainer>
+  );
+};
